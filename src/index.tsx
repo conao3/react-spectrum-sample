b13268d4c8b6044905f0a1faa1e5006154712cd9
Naoya Yamashita <conao3@gmail.com>
2021-11-02 00:39:34 +0900

    hello react-spectrum

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
-import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-export const App: React.FC = () => {
-    return <h1>Hello React</h1>;
-};
+import {Provider, defaultTheme, Button} from '@adobe/react-spectrum';
+
+function App() {
+  return (
+    <Provider theme={defaultTheme}>
+      <Button variant="cta" onPress={() => alert('Hey there!')}>
+        Hello React Spectrum!
+      </Button>
+    </Provider>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
